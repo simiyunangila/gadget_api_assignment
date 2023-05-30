c3f9426e01841cbe4bacd40bfcb0a0616c70fc8c
simiyunangila <srebeccnangila@gmail.com>
2023-05-30 19:05:49 +0300

    commit

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 let productsContainer = document.getElementById('products');
 const getProducts = ()=>{
     return fetch('https://dummyjson.com/products?limit=8')
-    // return fetch('https://dummyjson.com/users?limit=5')
     .then(response =>response.json())
     .then(response=>response)
     .catch(error=>error.message)
-    // .then(response=>console.log({response}))
-    // .catch(error=>console.log(error))
 };
 getProducts();
 
@@ -19,69 +16,20 @@ products.products.map(item=>{
     let img = document.createElement('img');
     let title= document.createElement('h2');
     let userName = document.createElement('p');
-    let discount =document.createElement('P')
     let cost =document.createElement('p')
-    let rate =document.createElement('p')
-    let stoc =document.createElement('p')
-    let thumb =document.createElement('p')
-// ​​
-// rating: 4.69
-// ​​
-// stock: 94
-// ​​
-// thumbnail
-
-    img.src = item.images;
+    
+    img.src = item.thumbnail;
     title.innerHTML = item.title;
     userName.innerHTML = item.description;
-    discount.innerHTML =item.discountPercentage;
     cost.innerHTML = item.price;
-    rate.innerHTML = item.rating;
-    stoc.innerHTML = item.stock;
-    thumb.innerHTML = item.thumbnail;
+  
 
     div.appendChild(img);
     div.appendChild(cost);
-    div.appendChild(rate);
-    div.appendChild(userName);
-    div.appendChild(discount);
-    div.appendChild(stoc);
-    div.appendChild(thumb);
+    div.appendChild(userName);;
     div.appendChild(title);
     productsContainer.appendChild(div);
 
 })
 }
 displayProducts();
-
-// function generateProductCards(data) {
-//     const productContainer = document.getElementById("product-container");
-//     // iterate through the data and create a card for each product
-//     data.forEach((product) => {
-//       // create a card element
-//       const card = document.createElement("div");
-//       card.classList.add("product-card");
-//       // Set the card content using the product data
-//       card.innerHTML = `
-//               <img src="${product.thumbnail}" alt="${product.name}" />
-//               <h2>${product.title}</h2>
-//               <div class='productPrice'>
-//               <p>${product.price}</p>
-//               <button>ADD TO CART</button>
-//               </div>
-//           `;
-//       // append the card to the product container
-//       productContainer.appendChild(card);
-//     });
-//   }
-//   function fetchProducts() {
-//     fetch("https://dummyjson.com/products")
-//       .then((response) => response.json())
-//       .then((data) => {
-//         // Get the first 8 products from the data
-//         const returnedData = data.products.slice(0, 8);
-//         // call the function to generate cards
-//         generateProductCards(returnedData);
-//       });
-//   }
-//   fetchProducts()
